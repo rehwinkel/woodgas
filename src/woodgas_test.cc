8651e4d82ecc63257808d7bc8dd2ad636a9ddad8
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2020-08-17 18:40:25 +0200

    added glad for opengl loading

--- src/woodgas_test.cc
+++ src/woodgas_test.cc
@@ -1,9 +1,29 @@
 #include <iostream>
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
 int main(int argc, char const *argv[])
 {
-    glfwInit();
+    if (!glfwInit())
+    {
+        throw std::runtime_error("failed to initialize GLEW");
+    }
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Title", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGL())
+    {
+        throw std::runtime_error("failed to initialize GLAD");
+    }
+
+    glClearColor(1.0, 0.0, 1.0, 1.0);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
     std::cout << "hey" << std::endl;
     return 0;
 }

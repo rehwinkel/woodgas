b1ebc58ebc58e8f5fe8086b48685d03a911dd5b2
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2020-09-10 09:16:29 +0200

    added real texture to render example

--- test/debug/render.cc
+++ test/debug/render.cc
@@ -1,13 +1,17 @@
 #include <render/render.h>
+#include <asset/asset.h>
 
 #include <cstdlib>
 
 int main(int argc, char const *argv[]) {
     logging::Logger logger;
+    asset::Assets assets(logger, "../test_resources");
+    asset::Image &pic = assets.load_image("test.png");
     render::Window window(640, 480, "hey", logger);
     render::Renderer renderer(window, logger);
 
-    render::Texture tex(64, 64, 4, (char *)std::malloc(16384));
+    render::Texture tex(pic.get_width(), pic.get_height(), pic.get_components(),
+                        (char *)pic.get_data());
     float ar = 640.0 / 480.0;
     renderer.upload_ortho(-1 * ar, 1 * ar, -1, 1, 0.1, 100);
 
